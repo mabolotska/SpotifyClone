b71e650feb779d9da33544eb3b553a491debe196
Maryna Bolotska <42913418+mabolotska@users.noreply.github.com>
2024-01-16 11:04:06 +0100

    NewReleaseCollectionViewCell

---
+++ SpotifyClone/ViewModels/FeaturedPlaylistCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  FeaturedPlaylistCollectionViewCell.swift
+//  SpotifyClone
+//
+//  Created by Maryna Bolotska on 16/01/24.
+//
+
+import Foundation
---
+++ SpotifyClone/ViewModels/NewReleaseCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  NewReleaseCollectionViewCell.swift
+//  SpotifyClone
+//
+//  Created by Maryna Bolotska on 16/01/24.
+//
+
+import Foundation
